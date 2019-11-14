e347cd777316e38ce574d60e09d211b10350b656
YoloChocoPie <chocopieyolo@gmail.com>
2019-11-14 15:27:10 +0700

    what am i doing

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new OOPCSEntities();
+            var expenditures = db.Expenditures.ToArray();
+
+            int sum = 0;
+
+            foreach (var expenditure in expenditures)
+            {
+                sum = sum + expenditure.Price;
+            }
+
+            // same ideal, diffrent way to code
+
+           // for (int i = 0; i < expenditures.Length; i++)
+          //  {
+          //      sum = sum + expenditures[i].Price; 
+          //  }
+
+            Console.WriteLine("So tien da su dung = " + sum);
+        }
+    }
+}
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/ViewMember.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    class ViewMember
+    {
+        this.id = member.id;
+            this.Fullname = member.Fullname;
+            if (member.Gender == true)
+                this.Gender = "Nam";
+            else
+                this.Gender = "Nu";
+            if(member.Birthday.HasValue)
+                this.Birthday = String.Format("{0}/{1}/{2}",
+                    member.Birthday.Value.Day,
+                    member.Birthday.Value.Month,
+                    member.Birthday.Value.Year;
+                    
+            if(member.Status.HasValue)
+                this.Status = member.Status.Value == true ? "Living" : "Decreased";
+            this.Living = member.Living;
+            this.Occupation = member.Occupation;
+            var db = new OPCSEntities();
+            if(member.Father_id.HasValue)
+            {
+                var father = db.Members.Find(member.Father_id.Value);
+                this.Father = father.Fullname;
+            }
+            if(member.Spouse_id.HasValue)
+            {
+                var spouse = db.Members.Find(member.Spouse_id.Value);
+                this.Spouse = spouse.Fullname;
+            }
+        }
+        public int id { get; set; }
+        public string Fullname { get; set; }
+        public string Gender { get; set; }
+        public Nullable<System.DateTime> Birthday { get; set; }
+        public Nullable<bool> Status { get; set; }
+        public string Hometown { get; set; }
+        public string Living { get; set; }
+        public string Occupation { get; set; }
+        public string Father { get; set; }
+        public string Mother { get; set; }
+        public string Spouse { get; set; }
+    
+    }
+}
+
+    }
+}
